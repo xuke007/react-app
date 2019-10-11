72e6a0e2ac4c64b99d5361ef169799bd1ee342f1
xuke <a363778817@gmail.com>
2019-10-11 13:59:19 +0800

    Updates

---
+++ precache-manifest.233150e918845673f4ae9360605e837e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a26299bf623ebc17be6b49f5945c040",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8030e7f2766d4dad5342",
+    "url": "/react-app/static/css/main.88ba1ff8.chunk.css"
+  },
+  {
+    "revision": "4c19e820e6b0703f4dd6",
+    "url": "/react-app/static/js/2.5db5300a.chunk.js"
+  },
+  {
+    "revision": "8030e7f2766d4dad5342",
+    "url": "/react-app/static/js/main.f1226948.chunk.js"
+  },
+  {
+    "revision": "53c881d509f6a68b5729",
+    "url": "/react-app/static/js/runtime-main.a572a2e1.js"
+  }
+]);
